c653b468e1d90c7dec5194e06c19cb209461179d
rishabh sharma <rishabh.sharma@frontdoorhome.com>
2022-04-10 04:00:02 +0530

    added fb verification meta tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ export default function Home() {
             <Head>
                 <title>Metvy</title>
                 <link rel="icon" href="/icons/ic_launcher.png" />
+                <meta name="facebook-domain-verification" content="zuixvg64rz4u5kk18djgkdfziedw2k" />
                 {/* <script src="//code.tidio.co/uhenqkwcqosun9a0nqc2o5b1xj10nzdp.js" async></script> */}
             </Head>
             <LandingScreen />
