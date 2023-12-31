072deb24ea5baddd672af4daba2b35071fecd144
Rishabh Sharma <rishabh.sharma@Rishabhs-MacBook-Air.local>
2023-12-31 20:39:34 +0530

    Added Integration with API

--- src/screens/muzoClass/MuzoCheckout.tsx
+++ src/screens/muzoClass/MuzoCheckout.tsx
@@ -1,182 +1,261 @@
 import * as React from "react";
-import styled, {css} from "styled-components";
+import styled, { css } from "styled-components";
 import { useRouter } from 'next/router'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Loader from "../../components/LoaderComponent/LoaderComponent"
+import ServerDown from "../../components/ServerDown/ServerDown"
+import apiConfig from "../../api-services/apiConfig";
 
 const MyComponent = (props) => {
-    const router = useRouter()
-    const { course,amount,discount } = router.query
-    const payable = parseInt(amount as string,10) - parseInt(discount as string,10)
-
-    const [username, setUsername] = useState('');
-    const [usernameError, setUsernameError] = useState(false);
+  const router = useRouter()
+  const { course_uuid } = router.query
 
 
-    const [userEmail, setUserEmail] = useState('');
-    const [userEmailError, setUserEmailError] = useState(false);
+  const [courseFetched, setCourseFetched] = useState(false)
+
+  const [username, setUsername] = useState('');
+  const [usernameError, setUsernameError] = useState(false);
+
+
+  const [userEmail, setUserEmail] = useState('');
+  const [userEmailError, setUserEmailError] = useState(false);
 
 
 
-    const [mobile, setMobile] = useState('');
-    const [mobileError , setMobileError] = useState(false);
+  const [mobile, setMobile] = useState('');
+  const [mobileError, setMobileError] = useState(false);
 
-    const [couponCode, setCouponCode] = useState('');
-    
+  const [couponCode, setCouponCode] = useState('');
 
-    const onProceedClicked = () => {
-        if (username === ''){
-            setUsernameError(true)
-        }
-        if (userEmail === ''){
-            setUserEmailError(true)
-        }
-        if (mobile === ''){
-            setMobileError(true)
-        }
-        if (username !== '' && userEmail !== '' && mobile !== ''){
-            // Proceed to checkout
-            // make an api call, get razorpay link and redirect user window to that link
-        }
+  const [serverDown, setServerDown] = useState(false)
+
+
+  const [payableAmount, setPayableAmount] = useState(0);
+  const [courseName, setCourseName] = useState('');
+
+  const getCourse = async () => {
+    try {
+      if (course_uuid) {
+        console.log(`Fetching course with uuid ${course_uuid}`)
+        const { data } = await apiConfig.get(`/api/muzoclass/courses?course_uuid=${course_uuid}`);
+        // data will be an array of course
+        // fetch first object and set its prince and name
+        const course = data[0]
+        setPayableAmount(parseInt(course.coursePrice, 10))
+        setCourseName(course.programName)
+        setCourseFetched(true)
+        setServerDown(false)
+      }
+
+    } catch (error) {
+      console.log(`Error While Fetching data from server ${error}`)
+      setCourseFetched(true)
+      setServerDown(true)
     }
+  };
 
 
+  useEffect(() => {
+    getCourse()
+  }, [course_uuid]);
+
+
+
+  const onProceedClicked = async () => {
+    if (username === '') {
+      setUsernameError(true)
+    }
+    if (userEmail === '' || !isEmailValid(userEmail)) {
+      setUserEmailError(true)
+    }
+    if (mobile === '' || !isPhoneNumberValid(mobile)) {
+      setMobileError(true)
+    }
+    if (username !== '' && userEmail !== '' && mobile !== '') {
+      // Proceed to checkout
+      // make an api call, get razorpay link and redirect user window to that link
+      try {
+        console.log(`Enrolling to course with uuid ${course_uuid}`)
+        setCourseFetched(false)
+        // setCourse fetched it just a flag  to show loader
+        const enroll_payload = {
+          "name": username,
+          "email": userEmail,
+          "phone": mobile,
+          "discountCoupon": couponCode,
+          "course_uuid": course_uuid
+        }
+        const { data } = await apiConfig.post(`/api/muzoclass/courses/pay`,enroll_payload);
+        const payment_link = data.paymentLink
+        window.location.href = payment_link
+      } catch (error) {
+        console.log(`Error While Enrolling user to course => error ${error}`)
+        setCourseFetched(true)
+        setServerDown(true)
+      }
+    }
+  }
+
+  const isEmailValid = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const isPhoneNumberValid = (phone) => {
+    const phoneRegex = /^[0-9]{10}$/;
+    return phoneRegex.test(phone);
+  };
+
+  if (serverDown) {
     return (
-        <Div>
-            <Div2>
-                <Img
+      <ServerDown />
+    )
+  }
+
+  if (!courseFetched) {
+    return (
+      <Loader />
+    )
+  }
+
+
+  return (
+    <Div>
+      <Div2>
+        <Img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/04b03be6d407dc3e73dfa704fb60c4619340f921bf04017adf0b3ebec6e18b4c?"
+        />
+        <Div3>
+          <Div4> {courseName} </Div4>
+          <Div5>
+            <Div6>Order Summary</Div6>
+            <Div7 />
+            <Div8>
+              <Div9>
+                <Div10>Amount</Div10>
+                {/* <Div11>Discount Applied</Div11> */}
+                {/* <Div12>Net Payable</Div12> */}
+              </Div9>
+              <Div13>
+                <Div14>&#8377; {payableAmount}</Div14>
+                {/* <Div15>--$ discount</Div15> */}
+                {/* <Div16>$ payable</Div16> */}
+              </Div13>
+            </Div8>
+          </Div5>
+          <Div17>
+            <Div18>Your Details</Div18>
+            <Div19 />
+            <Div20>Name </Div20>
+            <Div25 error={usernameError} >
+              <Div26>
+                <Div27>
+                  <input
+                    type="text"
+                    placeholder="Enter Name"
+                    value={username}
+                    onChange={(e) => {
+                      if (usernameError) {
+                        setUsernameError(false)
+                      }
+                      setUsername(e.target.value)
+                    }}
+                    style={{ width: '100%' }}
+                  />
+                </Div27>
+                {usernameError && (
+                  <Img2
                     loading="lazy"
-                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/04b03be6d407dc3e73dfa704fb60c4619340f921bf04017adf0b3ebec6e18b4c?"
-                />
-                <Div3>
-                    <Div4> MuzoClass {course} Course</Div4>
-                    <Div5>
-                        <Div6>Order Summary</Div6>
-                        <Div7 />
-                        <Div8>
-                            <Div9>
-                                <Div10>Amount</Div10>
-                                <Div11>Discount Applied</Div11>
-                                <Div12>Net Payable</Div12>
-                            </Div9>
-                            <Div13>
-                                <Div14>${amount}</Div14>
-                                <Div15>--${discount}</Div15>
-                                <Div16>${payable}</Div16>
-                            </Div13>
-                        </Div8>
-                    </Div5>
-                    <Div17>
-                        <Div18>Your Details</Div18>
-                        <Div19 />
-                        <Div20>Name </Div20>
-                        <Div25 error = {usernameError} >
-                            <Div26>
-                                <Div27>
-                                    <input
-                                        type="text"
-                                        placeholder="Enter Name"
-                                        value={username}
-                                        onChange={(e) => {
-                                            if (usernameError){
-                                                setUsernameError(false)
-                                            }
-                                            setUsername(e.target.value)
-                                        }}
-                                        style={{ width: '100%' }}
-                                     />
-                                </Div27>
-                                {usernameError && (
-                                    <Img2
-                                    loading="lazy"
-                                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/5ccc26304400869a3509df6d480bda813e2e3cff19daf767d13e5b5c60863a0b?"
-                                 />
-                                )}
-                            </Div26>
-                        </Div25>
-                        {usernameError && (
-                            <Div28>Please enter a valid name</Div28>
-                        )}
-                        <Div22>E-mail Id </Div22>
-                        <Div25 error = {userEmailError} >
-                            <Div26>
-                                <Div27>
-                                    <input
-                                        type="text"
-                                        placeholder="Enter Email"
-                                        value={userEmail}
-                                        onChange={(e) => {
-                                            if (userEmailError){
-                                                setUserEmailError(false)
-                                            }
-                                            setUserEmail(e.target.value)
-                                        }}
-                                        style={{ width: '100%' }}
-                                     />
-                                </Div27>
-                                {userEmailError && (
-                                    <Img2
-                                    loading="lazy"
-                                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/5ccc26304400869a3509df6d480bda813e2e3cff19daf767d13e5b5c60863a0b?"
-                                 />
-                                )}
-                            </Div26>
-                        </Div25>
-                        {userEmailError && (
-                            <Div28>Please enter a valid email</Div28>
-                        )}
-                        <Div24>Mobile Number</Div24>
-                        <Div25 error = {mobileError} >
-                            <Div26>
-                                <Div27>
-                                    <input
-                                        type="text"
-                                        placeholder="Enter Mobile Number"
-                                        value={mobile}
-                                        onChange={(e) => {
-                                            if (mobileError){
-                                                setMobileError(false)
-                                            }
-                                            setMobile(e.target.value)
-                                        }}
-                                        style={{ width: '100%' }}
-                                     />
-                                </Div27>
-                                {mobileError && (
-                                    <Img2
-                                    loading="lazy"
-                                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/5ccc26304400869a3509df6d480bda813e2e3cff19daf767d13e5b5c60863a0b?"
-                                 />
-                                )}
-                            </Div26>
-                        </Div25>
-                        {mobileError && (
-                            <Div28>Please enter a valid phone no.</Div28>
-                        )}
-                        <Div29>Coupon Code</Div29>
-                        <Div30>
-                            <input
-                                type="text"
-                                placeholder="Enter Coupon Code"
-                                value={couponCode}
-                                onChange={(e) => {
-                                    setCouponCode(e.target.value)
-                                }}
-                                style={{ width: '100%' }}
-                            />
-                        </Div30>
-                        <Div31 onClick={onProceedClicked} >
-                            <Div32>Proceed to checkout </Div32>
-                        </Div31>
-                    </Div17>
-                </Div3>
-            </Div2>
-        </Div>
-    );
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/5ccc26304400869a3509df6d480bda813e2e3cff19daf767d13e5b5c60863a0b?"
+                  />
+                )}
+              </Div26>
+            </Div25>
+            {usernameError && (
+              <Div28>Please enter a valid name</Div28>
+            )}
+            <Div22>E-mail Id </Div22>
+            <Div25 error={userEmailError} >
+              <Div26>
+                <Div27>
+                  <input
+                    type="text"
+                    placeholder="Enter Email"
+                    value={userEmail}
+                    onChange={(e) => {
+                      if (userEmailError) {
+                        setUserEmailError(false)
+                      }
+                      setUserEmail(e.target.value)
+                    }}
+                    style={{ width: '100%' }}
+                  />
+                </Div27>
+                {userEmailError && (
+                  <Img2
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/5ccc26304400869a3509df6d480bda813e2e3cff19daf767d13e5b5c60863a0b?"
+                  />
+                )}
+              </Div26>
+            </Div25>
+            {userEmailError && (
+              <Div28>Please enter a valid email</Div28>
+            )}
+            <Div24>Mobile Number</Div24>
+            <Div25 error={mobileError} >
+              <Div26>
+                <Div27>
+                  <input
+                    type="text"
+                    placeholder="Enter Mobile Number"
+                    value={mobile}
+                    onChange={(e) => {
+                      if (mobileError) {
+                        setMobileError(false)
+                      }
+                      setMobile(e.target.value)
+                    }}
+                    style={{ width: '100%' }}
+                  />
+                </Div27>
+                {mobileError && (
+                  <Img2
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/5ccc26304400869a3509df6d480bda813e2e3cff19daf767d13e5b5c60863a0b?"
+                  />
+                )}
+              </Div26>
+            </Div25>
+            {mobileError && (
+              <Div28>Please enter a valid phone no.</Div28>
+            )}
+            <Div29>Coupon Code</Div29>
+            <Div30>
+              <input
+                type="text"
+                placeholder="Enter Coupon Code"
+                value={couponCode}
+                onChange={(e) => {
+                  setCouponCode(e.target.value)
+                }}
+                style={{ width: '100%' }}
+              />
+            </Div30>
+            <Div31 onClick={onProceedClicked} >
+              <Div32>Proceed to checkout </Div32>
+            </Div31>
+          </Div17>
+        </Div3>
+      </Div2>
+    </Div>
+  );
 }
 
 
 interface DivProps {
-    error?: boolean;
+  error?: boolean;
 }
 
 const Div = styled.div`
